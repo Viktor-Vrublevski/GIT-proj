266e119202891a8e45714dc6694895e2b6f1674c
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-07-30 17:59:12 +0300

    Create class Dog
    service update-2
    
    update
    
    update
    
    update

---
+++ src/main/java/com/example/git_study/service/DogService.java
@@ -0,0 +1,11 @@
+package com.example.git_study.service;
+
+import com.example.git_study.entity.Dog;
+
+public class DogService {
+    private Dog dog;
+
+    private Dog getDog(int id) {
+        return new Dog();
+    }
+}
