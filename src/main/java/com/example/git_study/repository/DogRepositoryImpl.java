7799a01deadfd21852015242193ffdc0e3b944bd
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-07-31 20:23:24 +0300

    Update methods for DogRepository
    update

--- src/main/java/com/example/git_study/repository/DogRepositoryImpl.java
+++ src/main/java/com/example/git_study/repository/DogRepositoryImpl.java
@@ -2,24 +2,35 @@ package com.example.git_study.repository;
 
 import com.example.git_study.entity.Dog;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DogRepositoryImpl implements DogRepository{
+
+    private static Map<Long, Dog> DOG_STORE = new HashMap<>();
+
     @Override
     public Dog getDogById(long id) {
-        return null;
+        return DOG_STORE.get(id);
     }
 
     @Override
-    public boolean addDog (Dog dog) {
-        return false;
+    public boolean addDog(Dog dog) {
+        if (DOG_STORE.containsKey(dog.getId())) {
+            return false;
+        } else {
+            DOG_STORE.put(dog.getId(), dog);
+            return true;
+        }
     }
 
     @Override
     public void delete(long id) {
-
+        DOG_STORE.remove(id);
     }
 
     @Override
     public void update(Dog dog) {
-
+        DOG_STORE.put(dog.getId(), dog);
     }
 }
