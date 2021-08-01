6dde1bd8b3f5c0dadd64f4237b3f7bb146814c26
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-08-01 11:21:50 +0300

    update2

--- src/main/java/com/example/git_study/controller/DogController.java
+++ src/main/java/com/example/git_study/controller/DogController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController(value = "/Dog")
@@ -26,7 +27,7 @@ public class DogController {
     }
 
     @GetMapping("/all-dogs")
-    public List<Dog> getAllDogs() {
-        return null;
+    public Collection<Dog> getAllDogs() {
+        return service.getAllDogs();
     }
 }
