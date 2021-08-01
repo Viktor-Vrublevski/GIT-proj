5dca852e49f0bec2b3939844cbd02521d2bf6523
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-08-01 11:11:44 +0300

    Add method getDogs to DogController
    update
    
    update2
    
    update 3

--- src/main/java/com/example/git_study/controller/DogController.java
+++ src/main/java/com/example/git_study/controller/DogController.java
@@ -5,11 +5,12 @@ import com.example.git_study.entity.Dog;
 import com.example.git_study.service.DogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
 
-@RestController(value = "/Dog")
+
+@RestController
+@RequestMapping("/Dog")
 public class DogController {
 
     private DogService service;
@@ -19,9 +20,15 @@ public class DogController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> addNewDog(@RequestParam Dog dog) {
+    public ResponseEntity<String> addNewDog(@RequestBody Dog dog) {
         if(service.create(dog)) {
             return new ResponseEntity<>(HttpStatus.OK);
         } else return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
+
+    @GetMapping("/all-dogs")
+    public Collection<Dog> getAllDogs() {
+        return service.getAllDogs();
+    }
+
 }
