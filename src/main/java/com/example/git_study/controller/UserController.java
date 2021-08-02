d10c1b30c862c1f09da4f053b2907ad2b835542b
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-08-02 08:28:48 +0300

    Create methods in UserController

--- src/main/java/com/example/git_study/controller/UserController.java
+++ src/main/java/com/example/git_study/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.git_study.controller;
 
+import com.example.git_study.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/User")
 public class UserController {
+
+    private UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
 }
