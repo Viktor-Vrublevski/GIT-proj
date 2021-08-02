63043cc7a37e3a24ae918dc663c2c408445fcdaf
Viktor-Vrublevski <zestaw2017@mail.ru>
2021-08-02 08:22:58 +0300

    Create UserController

---
+++ src/main/java/com/example/git_study/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.git_study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/User")
+public class UserController {
+
+}
